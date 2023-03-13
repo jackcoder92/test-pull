e314754cff9326f4d47bc2fd2acbe4f44a680c3b
ibrataha8 <115188113+ibrataha8@users.noreply.github.com>
2023-03-13 17:55:31 +0100

    change someting (#6)
    Co-authored-by: jackcoder92 <120723432+jackcoder92@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log("test get yolo")
+function factorial(n) {
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  
+  let number = 5;
+  console.log(`The factorial of ${number} is: ${factorial(number)}`);
+  
